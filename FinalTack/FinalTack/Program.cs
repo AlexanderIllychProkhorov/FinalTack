e7d82758450ae7e07bd77dfa74b524ebd198e3f6
Alexander Prokhorov <b1.prokhorov.alexandr@gmail.com>
2018-05-21 19:24:39 +0200

    Commit

--- FinalTack/FinalTack/Program.cs
+++ FinalTack/FinalTack/Program.cs
@@ -3,39 +3,68 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace FinalTasck
 {
     class Program
     {
+        public struct MyThreadArg
+        {
+            public MyThreadArg(string path) { Path = path; Mtx = new Mutex(); }
+            public string Path;
+            public Mutex Mtx;
+        }
+        static void MyThreadFunc(object arg)
+        {
+            var threadArgs = ((MyThreadArg)arg);
+
+            var directoryList = new DirectoryInfo(threadArgs.Path);
+            Console.WriteLine("List of folders: ");
+            foreach (var dir in directoryList.GetDirectories())
+            {
+                {
+                    Console.WriteLine(dir.Name);
+                    Console.WriteLine("List of subfolders: ");
+                    foreach (var sdir in dir.GetDirectories())
+                    {
+                        threadArgs.Mtx.WaitOne();
+                        Thread.Sleep(100);
+                        Console.WriteLine(sdir.Name);
+                        Console.WriteLine();
+                        threadArgs.Mtx.ReleaseMutex();
+                    }
+                }
+            }
+        }
         static void Main(string[] args)
         {
+            var subdir = string.Empty;
             Console.BackgroundColor = ConsoleColor.DarkGreen;
-            try
-            {
-                Console.WriteLine("Please input path to the directory");
-                var path = Console.ReadLine();
-                DirectoryInfo directoryList = new DirectoryInfo(path);
-                List<DirectoryInfo> list = new List<DirectoryInfo>();
-                list.Add(directoryList);
-                var directoryList1 = Directory.GetDirectories(path);
-                if (directoryList != null)
-                {
-                 //   foreach (string dir in directoryList)
 
-                    //    Console.WriteLine(dir);
-                    Console.ReadKey();
-                }
-                else
-                {
-                    Console.WriteLine("Directory List is null.Please input correct path");
-                }
-            }
-            catch (Exception ex)
+            Console.WriteLine("Please input path to the directory");
+            var path = Console.ReadLine();
+            Thread myThread = new Thread(MyThreadFunc);
+            var threaedArgs = new MyThreadArg(path);
+            myThread.Start(threaedArgs);
+            while (myThread.ThreadState != ThreadState.Stopped)
             {
-                Console.WriteLine(ex);
+                if (Console.ReadKey().Key == ConsoleKey.Spacebar)
+                {
+                    threaedArgs.Mtx.WaitOne();
+                    while (true)
+                    {
+                        if (Console.ReadKey().Key == ConsoleKey.Spacebar)
+                        {
+                            threaedArgs.Mtx.ReleaseMutex();
+                            break;
+                        }
+                    }
+                }
             }
+            myThread.Join();
+
         }
     }
 }
